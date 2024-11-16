d5739ffb35855008710ca8727876d2844783efb2
Linus Lam <linuslyt@gmail.com>
2024-11-15 16:28:08 -0800

    Initialize Vite template. Add ESLint and Prettier configs.

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier, // Enables Prettier plugin
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // Show Prettier formatting issues as errors in ESLint
+    },
+    extends: [
+      'eslint:recommended', // ESLint's recommended set of rules
+      'plugin:react/recommended', // React-specific linting rules
+      'prettier', // Turn off all ESLint rules that are unnecessary or might conflict with Prettier
+      'plugin:prettier/recommended', // Ensure Prettier is working and errors are shown in ESLint
+    ],
+  },
+];
